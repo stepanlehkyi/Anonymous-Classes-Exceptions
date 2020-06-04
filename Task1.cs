ec9a9fa938cf8bde4b5ecbaf59536a8eb7e7f503
stepanlehkyi <60749566+stepanlehkyi@users.noreply.github.com>
2020-06-04 15:49:20 +0300

    Task1 & Task2

---
+++ Task1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+/*Create method Div(), which calculates the dividing of two int numbers. In
+Main read two int numbers and call this method. Catch appropriative
+exceptions. Throwing exception if dividing of two double numbers.*/
+namespace Homework_Exeptions
+{
+    class Program
+    {
+        static int Div(int numberOne, int numberTwo)=> numberOne / numberTwo;
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a first number:");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number:");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine(Div(numberOne, numberTwo));
+            }
+            catch (DivideByZeroException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch(FormatException exception) when (numberOne.GetType() == typeof(double) && numberTwo.GetType() == typeof(double))
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (ArithmeticException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception) 
+            {
+                Console.WriteLine(exception.Message);
+            }
+           
+
+
+        }
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+/*Write a method ReadNumber(int start, int end), which reads from Console
+integer numbers and returns it, if it is in the range [start...end]. If an invalid
+number or non-number text is read, the method should throw an exception.
+Using this method write a method Main(), that has to enter 10 numbers:
+
+a1, a2, ..., a10, such that 1 < a1 < ... < a10 < 100*/
+namespace Task_2
+{
+    class Program
+    {
+        public static int ReadNumber(int start,int end)
+        {
+            int number = 0;
+            try
+            {
+                Console.WriteLine("Enter a number");
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+          
+            for(int i = start; i <= end; i++)
+            {
+                if(number == i)
+                {
+                    return number;
+                }
+            }
+            return 0;
+            
+        } 
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+          
+                list.Add(ReadNumber(1, 100));
+                for (int i = 1; i < 10; i++)
+                {
+                    list.Add((ReadNumber(1, 100)));
+                    if ((list[i] <= list[i - 1]))
+                    {
+                        throw new Exception("The sequence has to accept a rule: every net value should be bigger than previous one ");
+                    }
+                }
+            Console.WriteLine("You entered: ");
+         foreach(var item in list)
+            {
+                Console.Write(item+",");
+            }
+            Console.WriteLine("\b.");
+        }
+    }
+}
